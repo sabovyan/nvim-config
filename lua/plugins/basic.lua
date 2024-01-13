12cde58420077cbc2251ac9bb026e94e94b814b0
sabovyan <abovyan.sargis90@gmail.com>
2024-01-13 13:18:58 +0400

    [setup] change whichkey

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -1,13 +1,21 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	config = function()
+		local wk = require("which-key")
+
+		wk.register({
+			g = { name = "[G]it" },
+			c = { name = "[C]ode [A]ction" },
+		}, { prefix = "<leader>" })
+	end,
 }
