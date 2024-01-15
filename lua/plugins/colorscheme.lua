684a01bf0a1be5e441bae142b79fd9d983dce064
sabovyan <abovyan.sargis90@gmail.com>
2024-01-15 12:35:15 +0400

    [COLORSCHEME] make tokio night

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,9 @@ return {
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
+    require("tokyonight").setup({
+      style = "night"
+    })
 		-- load the colorscheme here
 		vim.cmd([[colorscheme tokyonight]])
 	end,
