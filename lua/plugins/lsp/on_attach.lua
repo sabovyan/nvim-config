cbcea71390c20887392b5c7e31d7a0cbfa56f780
sabovyan <abovyan.sargis90@gmail.com>
2024-01-16 02:00:26 +0400

    [ON_ATTACH] fix keymap

--- lua/plugins/lsp/on_attach.lua
+++ lua/plugins/lsp/on_attach.lua
@@ -1,4 +1,5 @@
 local M = {}
+
 function M.on_attach(_, bufnr)
 	-- NOTE: Remember that lua is a real programming language, and as such it is possible
 	-- to define small helper and utility functions so you don't have to repeat yourself
@@ -6,76 +7,80 @@ function M.on_attach(_, bufnr)
 	--
 	-- In this case, we create a function that lets us more easily define mappings specific
 	-- for LSP related items. It sets the mode, buffer and description for us each time.
-	local nmap = function(keys, func, desc)
+	local nmap = function(keys, func, desc, mode)
 		if desc then
 			desc = "LSP: " .. desc
 		end
 
-		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+		vim.keymap.set(mode or "n", keys, func, { buffer = bufnr, desc = desc })
 	end
+
+	print("setLsp_keymap")
 
 	-- Global mappings.
 	-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-	vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 
-	nmap("<leader>cr", vim.lsp.buf.rename, "[R]e[n]ame")
-	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
+	nmap("<leader>cl", "<cmd>LspInfo<cr>", "Lsp Info")
 	nmap(
 		"gd",
-		require("telescope.builtin").lsp_definitions,
-		"[G]oto [D]efinition"
+		vim.lsp.buf.definition,
+		--  function()
+		--	require("telescope.builtin").lsp_definitions({ reuse_win = true })
+		--  end,
+		"Goto Definition"
 	)
-	nmap(
-		"gr",
-		require("telescope.builtin").lsp_references,
-		"[G]oto [R]eferences"
-	)
+
+	nmap("gr", "<cmd>Telescope lsp_references<cr>", "References")
+	nmap("gD", vim.lsp.buf.declaration, "Goto Declaration")
 	nmap(
 		"gI",
-		require("telescope.builtin").lsp_implementations,
-		"[G]oto [I]mplementation"
+		vim.lsp.buf.implementation,
+		-- function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,
+		"Goto Implementation"
 	)
-	nmap(
-		"<leader>D",
-		require("telescope.builtin").lsp_type_definitions,
-		"Type [D]efinition"
-	)
-	nmap(
-		"<leader>ds",
-		require("telescope.builtin").lsp_document_symbols,
-		"[D]ocument [S]ymbols"
-	)
+	nmap("gy", function()
+		require("telescope.builtin").lsp_type_definitions({ reuse_win = true })
+	end, "Goto T[y]pe Definition")
+	nmap("K", vim.lsp.buf.hover, "Hover")
+	nmap("gK", vim.lsp.buf.signature_help, "Signature Help")
+	nmap("<c-k>", vim.lsp.buf.signature_help, "Signature Help", "i")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "Code Action", { "n", "v" })
+	nmap("<leader>cA", function()
+		vim.lsp.buf.code_action({ context = { only = { "source" }, diagnostics = {} } })
+	end, "Source Action")
+	nmap("<leader>cr", vim.lsp.buf.rename, "Rename")
+
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+	-- diagnostic
+	local diagnostic_goto = function(next, severity)
+		local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+		severity = severity and vim.diagnostic.severity[severity] or nil
+		return function()
+			go({ severity = severity })
+		end
+	end
+	nmap("<leader>cd", vim.diagnostic.open_float, "Line Diagnostics")
+	nmap("]d", diagnostic_goto(true), "Next Diagnostic")
+	nmap("[d", diagnostic_goto(false), "Prev Diagnostic")
+	nmap("]e", diagnostic_goto(true, "ERROR"), "Next Error")
+	nmap("[e", diagnostic_goto(false, "ERROR"), "Prev Error")
+	nmap("]w", diagnostic_goto(true, "WARN"), "Next Warning")
+	nmap("[w", diagnostic_goto(false, "WARN"), "Prev Warning")
+
 	nmap(
 		"<leader>ws",
 		require("telescope.builtin").lsp_dynamic_workspace_symbols,
 		"[W]orkspace [S]ymbols"
 	)
 
-	-- See `:help K` for why this keymap
-	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-	-- Lesser used LSP functionality
-	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-	nmap(
-		"<leader>wa",
-		vim.lsp.buf.add_workspace_folder,
-		"[W]orkspace [A]dd Folder"
-	)
-	nmap(
-		"<leader>wr",
-		vim.lsp.buf.remove_workspace_folder,
-		"[W]orkspace [R]emove Folder"
-	)
 	nmap("<leader>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, "[W]orkspace [L]ist Folders")
 
 	-- Create a command `:Format` local to the LSP buffer
 	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		print("formatting", _.name)
 		vim.lsp.buf.format()
 	end, { desc = "Format current buffer with LSP" })
 end
