1df3de83e4b5ea85aee2b0b3f8bde56870280eb6
sabovyan <abovyan.sargis90@gmail.com>
2024-01-16 18:54:52 +0400

    [FORMAT] Add Disable/Enable Formatting

--- lua/configs/autocmd.lua
+++ lua/configs/autocmd.lua
@@ -9,3 +9,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+-- NOTE: this is for Conform.nvim
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		vim.b.disable_autoformat = true
+		print("Auto Format Disabled For This Buffer")
+	else
+		vim.g.disable_autoformat = true
+
+		print("Auto Format Disabled Globally")
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+	print("Auto Format Enabled")
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,10 +1,54 @@
 local Utils = require("utils")
+
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+
 	dependancies = {
 		"williamboman/mason.nvim",
 	},
-	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{
+			"<leader>ufd",
+			function()
+				vim.cmd.FormatDisable()
+			end,
+			mode = { "n", "v" },
+			desc = "[A]uto [F]ormat [D]isable",
+		},
+
+		{
+			"<leader>ufb",
+			function()
+				vim.cmd.FormatDisable({ bang = true })
+			end,
+			mode = { "n", "v" },
+			desc = "[B]uffer [A]uto [F]ormat [D]isable",
+		},
+		{
+			mode = { "n", "v" },
+			"<leader>ufe",
+			function()
+				vim.cmd("FormatEnable")
+			end,
+			desc = "[A]uto [F]ormat [E]nable",
+		},
+
+		{
+
+			mode = { "n", "v" },
+			"<leader>cf",
+			function()
+				require("conform").format({
+					lsp_fallback = true,
+					async = true,
+					timeout_ms = 500,
+				})
+			end,
+			{ desc = "Format file or range (in visual mode)" },
+		},
+	},
 	config = function()
 		local conform = require("conform")
 
@@ -23,11 +67,13 @@ return {
 				graphql = { "prettier" },
 				lua = { "stylua" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_fallback = true }
+			end,
 		})
 
 		local packages = {
@@ -36,13 +82,5 @@ return {
 		}
 
 		Utils.install_LSP(packages)
-
-		vim.keymap.set({ "n", "v" }, "<leader>cf", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
