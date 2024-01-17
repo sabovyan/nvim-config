305c589f036c72b957abac5e0559dbe2004e97cf
sabovyan <abovyan.sargis90@gmail.com>
2024-01-18 00:18:13 +0400

    [LINT] add linter

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -17,8 +17,7 @@ return {
 			typescript = { "eslint" },
 			javascriptreact = { "eslint" },
 			typescriptreact = { "eslint" },
-			svelte = { "eslint" },
-			markdown = { "vale" },
+			-- svelte = { "eslint" },
 		}
 
 		local packages = {
@@ -28,9 +27,28 @@ return {
 
 		Utils.install_LSP(packages)
 
+		local lspconfig = require("lspconfig")
+
+		lspconfig.eslint.setup({
+			settings = {
+				workingDirectory = { mode = "auto" },
+				codeActionOnSave = {
+					enable = true,
+					mode = "all",
+				},
+				format = true,
+			},
+			on_attach = function(_, bufnr)
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = bufnr,
+					command = "EslintFixAll",
+				})
+			end,
+		})
+
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
 			group = lint_augroup,
 			callback = function()
 				lint.try_lint()
@@ -38,3 +56,63 @@ return {
 		})
 	end,
 }
+
+-- ["eslint"] = function()
+-- 	local lspconfig = require("lspconfig")
+-- 	lspconfig.eslint.setup({
+
+-- 		on_attach = function(client, bufnr)
+-- 			print("eslint on_attach")
+-- 			Util.set_Lsp_keymap()
+
+-- 			-- Use EslintFixAll on Neovim < 0.10.0
+-- 			local diag = vim.diagnostic.get(
+-- 				bufnr,
+-- 				{ namespace = vim.lsp.diagnostic.get_namespace(client.id) }
+-- 			)
+-- 			if #diag > 0 then
+-- 				vim.cmd("EslintFixAll")
+-- 			end
+
+-- 			-- activate formatting
+-- 			--  if client.resolved_capabilities.document_formatting then
+-- 			--    vim.cmd("autocmd BufWritePre <buffer> eslint --fix <buffer>")
+-- 			--  end
+
+-- 			--	if client.name == "eslint" then
+-- 			--		client.server_capabilities.documentFormattingProvider = true
+-- 			--	elseif client.name == "tsserver" then
+-- 			--		client.server_capabilities.documentFormattingProvider = false
+-- 			--	end
+
+-- 			--	local diag = vim.diagnostic.get(
+-- 			--		bufnr,
+-- 			--		{ namespace = vim.lsp.diagnostic.get_namespace(client.id) }
+-- 			--	)
+
+-- 			--	if #diag > 0 then
+-- 			--		vim.cmd("EslintFixAll")
+-- 			--	end
+
+-- 			--	vim.api.nvim_create_autocmd("BufWritePre", {
+-- 			--		pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
+-- 			--		command = "silent! EslintFixAll",
+-- 			--		group = vim.api.nvim_create_augroup(
+-- 			--			"MyAutocmdsJavaScripFormatting",
+-- 			--			{}
+-- 			--		),
+-- 			--	})
+-- 		end,
+-- 		capabilities = capabilities,
+-- 		settings = {
+
+-- 			-- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+-- 			workingDirectory = { mode = "auto" },
+-- 			codeActionOnSave = {
+-- 				enable = true,
+-- 				mode = "all",
+-- 			},
+-- 			format = true,
+-- 		},
+-- 	})
+-- end,
