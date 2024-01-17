29252cf490bb6db7e27ed84e167bc646c902ef70
sabovyan <abovyan.sargis90@gmail.com>
2024-01-17 20:50:10 +0400

    [TREESITTER] change setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,56 +1,61 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  build = ':TSUpdate',
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	build = ":TSUpdate",
 
-  config = function()
+	config = function()
+		-- [[ Configure Treesitter ]]
+		-- See `:help nvim-treesitter`
+		require("nvim-treesitter.configs").setup({
+			-- NOTE: this are default values
+			modules = {},
+			sync_install = false,
+			auto_install = false,
+			ignore_install = {},
+			-- NOTE: end
 
-    -- [[ Configure Treesitter ]]
-    -- See `:help nvim-treesitter`
-    require('nvim-treesitter.configs').setup ({
-      ensure_installed = {
-        "lua",
-        "tsx",
-        "json",
-        'javascript',
-        'typescript',
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "vim",
-        "gitignore",
-        "query",
-        -- 'rust',
-        'vimdoc',
-        "dockerfile",
-      },
-      highlight = {
-        enable = true ,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<M-space>',
-        },
-      },
-    })
-  end
-
+			ensure_installed = {
+				"lua",
+				"tsx",
+				"json",
+				"javascript",
+				"typescript",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"vim",
+				"gitignore",
+				"query",
+				-- 'rust',
+				"vimdoc",
+				"dockerfile",
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+			context_commentstring = {
+				enable = true,
+				enable_autocmd = false,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = "<c-s>",
+					node_decremental = "<M-space>",
+				},
+			},
+		})
+	end,
 }
