7042ff4ee74398def9e9508993a61664afb291f1
sabovyan <abovyan.sargis90@gmail.com>
2024-01-19 16:25:20 +0400

    [syntax] add headlines.nvim

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -1,50 +1,55 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	-- dependencies = {
-	-- 	"nvim-treesitter/nvim-treesitter-textobjects",
-	-- },
-	build = ":TSUpdate",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		-- dependencies = {
+		-- 	"nvim-treesitter/nvim-treesitter-textobjects",
+		-- },
+		build = ":TSUpdate",
 
-	config = function()
-		-- [[ Configure Treesitter ]]
-		-- See `:help nvim-treesitter`
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"tsx",
-				"json",
-				"javascript",
-				"typescript",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"vim",
-				"gitignore",
-				"query",
-				-- 'rust',
-				"vimdoc",
-				"dockerfile",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<c-space>",
-					node_incremental = "<c-space>",
-					scope_incremental = "<c-s>",
-					node_decremental = "<M-space>",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"tsx",
+					"json",
+					"javascript",
+					"typescript",
+					"yaml",
+					"html",
+					"css",
+					"prisma",
+					"markdown",
+					"markdown_inline",
+					"svelte",
+					"graphql",
+					"bash",
+					"vim",
+					"gitignore",
+					"query",
+					"vimdoc",
+					"dockerfile",
 				},
-			},
-		})
-	end,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<c-space>",
+						node_incremental = "<c-space>",
+						scope_incremental = "<c-s>",
+						node_decremental = "<M-space>",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"lukas-reineke/headlines.nvim",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = true, -- or `opts = {}`
+		ft = { "markdown", "norg", "rmd", "org" },
+	},
 }
