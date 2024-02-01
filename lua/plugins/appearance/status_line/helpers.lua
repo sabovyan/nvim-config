baf325598bec2f012e614737e26c4300c58ba42d
sabovyan <abovyan.sargis90@gmail.com>
2024-02-01 19:55:22 +0400

    [status_line] modify file_name

---
+++ lua/plugins/appearance/status_line/helpers.lua
@@ -0,0 +1,74 @@
+---@param file_name string
+---@return boolean
+local function should_display_full_path(file_name)
+	local exception_files = {
+		"index.js",
+		"index.tsx",
+		"index.jsx",
+		"index.ts",
+		"init.lua",
+	}
+
+	return vim.tbl_contains(exception_files, file_name)
+end
+
+---@return string
+local function get_file_modify_status()
+	local modified = vim.fn.getbufvar(vim.fn.bufnr(), "&mod")
+	local readonly = vim.fn.getbufvar(vim.fn.bufnr(), "&readonly")
+	if modified == 1 then
+		return ""
+	elseif readonly == 1 then
+		return ""
+	else
+		return ""
+	end
+end
+
+local M = {}
+
+function M.branch_name()
+	local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+
+	if branch ~= "" then
+		if #branch > 10 then
+			local shortened_branch = string.sub(branch, 1, 10)
+			return " " .. shortened_branch .. "..."
+		else
+			return " " .. branch
+		end
+	else
+		return ""
+	end
+end
+
+function M.get_file_type()
+	local extension = vim.fn.expand("%:e")
+	local filetype = vim.fn.expand("%:t")
+
+	local icon = require("nvim-web-devicons").get_icon(filetype, extension)
+
+	if icon == nil then
+		icon = "[ICON NOT FOUND]"
+	end
+
+	return extension .. " " .. icon
+end
+
+function M.get_file_name()
+	local modifiedStatus = get_file_modify_status()
+
+	local file_name = vim.fn.expand("%:t")
+
+	if should_display_full_path(file_name) then
+		-- full path based on cw
+		local file_full_path = vim.fn.expand("%")
+		file_name = file_full_path
+	elseif file_name == "" then
+		file_name = "[No Name]"
+	end
+
+	return file_name .. " " .. modifiedStatus
+end
+
+return M
