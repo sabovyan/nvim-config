88c3a50e0750b1dfdbe884f30080506ff2a867af
sabovyan <abovyan.sargis90@gmail.com>
2024-02-08 19:32:54 +0400

    remove headline.nvim

--- lua/plugins/editor/syntax-hilighting.lua
+++ lua/plugins/editor/syntax-hilighting.lua
@@ -52,21 +52,4 @@ return {
 			})
 		end,
 	},
-	{
-		"lukas-reineke/headlines.nvim",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		opts = {
-			markdown = {
-				headline_highlights = {
-					"Headline1",
-					"Headline2",
-					"Headline3",
-					"Headline4",
-					"Headline5",
-					"Headline6",
-				},
-			},
-		},
-		ft = { "markdown", "norg", "rmd", "org" },
-	},
 }
