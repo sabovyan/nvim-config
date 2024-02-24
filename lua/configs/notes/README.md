4a4a6073c1cf054887a34a3577995065fca30e24
sabovyan <abovyan.sargis90@gmail.com>
2024-02-24 11:20:23 +0400

    [[add notes]]

---
+++ lua/configs/notes/README.md
@@ -0,0 +1,28 @@
+# Note taking in NeoVim
+
+- Motivation
+- Installation
+- Usage
+- Configuration
+- Credits
+- License
+
+## Usage
+
+**Open a todo list**
+
+```vim
+Note
+```
+
+**Open Note**
+
+```vim
+Note <note name>
+```
+
+**List notes**
+
+```vim
+Note list
+```
--- lua/configs/notes/init.lua
+++ lua/configs/notes/init.lua
@@ -1,11 +1,14 @@
 local notes_utils = require("configs.notes.utils")
 
+-- NOTE: temp solution
+local fallback_notes_dir = notes_utils.get_home_dir() .. "/notes"
+
 ---
 --- @param notes_dir? string - if not provided, it will be created in $HOME/notes
 ---@return string
 local create_note_dir = function(notes_dir)
 	local home_dir = notes_utils.get_home_dir()
-	notes_dir = notes_dir or home_dir .. "/notes"
+	notes_dir = notes_dir or fallback_notes_dir
 
 	local project_name = notes_utils.get_project_name()
 	local path_to_notes = notes_dir .. "/" .. project_name
@@ -15,14 +18,17 @@ local create_note_dir = function(notes_dir)
 	return path_to_notes
 end
 
-local open_todo_file = function(note_dir)
-	local todo_file = note_dir .. "/todo.md"
-	vim.cmd("e " .. todo_file)
+if vim.fn.getcwd() ~= fallback_notes_dir then
+	--- Open todo file in notes directory
+	---@param note_dir string
+	local open_todo_file = function(note_dir)
+		local todo_file = note_dir .. "/todo.md"
+		vim.cmd("e " .. todo_file)
+	end
+
+	vim.api.nvim_create_user_command("Notes", function()
+		local note_dir = create_note_dir()
+
+		open_todo_file(note_dir)
+	end, {})
 end
-
---- Current user command is only responsible for opening tood file
-vim.api.nvim_create_user_command("Notes", function()
-	local note_dir = create_note_dir()
-
-	open_todo_file(note_dir)
-end, {})
