8c8926d28b8fc89ac6f60c583c9d17336af2d038
sabovyan <abovyan.sargis90@gmail.com>
2024-03-01 17:55:46 +0400

    [[editor]] add template string

---
+++ lua/plugins/editor/template-string.lua
@@ -0,0 +1,15 @@
+return {
+	"axelvc/template-string.nvim",
+	config = true,
+	opts = {},
+	ft = {
+		"html",
+		"typescript",
+		"javascript",
+		"typescriptreact",
+		"javascriptreact",
+		"vue",
+		"svelte",
+		"python",
+	},
+}
