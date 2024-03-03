845ae42045dbc354e9bcf42e1f9d40167d02f0d9
sabovyan <abovyan.sargis90@gmail.com>
2024-03-03 15:14:19 +0400

    [[treesitter]] add auto install

--- lua/plugins/editor/syntax-hilighting.lua
+++ lua/plugins/editor/syntax-hilighting.lua
@@ -38,6 +38,7 @@ return {
 					-- NOTE: project specific languages
 					"php",
 				},
+				auto_install = true,
 				highlight = {
 					enable = true,
 					additional_vim_regex_highlighting = false,
