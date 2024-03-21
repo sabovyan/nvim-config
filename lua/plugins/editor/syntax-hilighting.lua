abe6561297c2d35f6052e05dabc1630d148c21a5
sabovyan <abovyan.sargis90@gmail.com>
2024-03-21 23:18:04 +0400

    [[syntax]] remove required languages

--- lua/plugins/editor/syntax-hilighting.lua
+++ lua/plugins/editor/syntax-hilighting.lua
@@ -1,15 +1,9 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		-- dependencies = {
-		-- 	"nvim-treesitter/nvim-treesitter-textobjects",
-		-- },
 		build = ":TSUpdate",
 
 		config = function()
-			vim.opt.foldmethod = "expr"
-			vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = {
 					-- NOTE: required by nvim-treesitter
@@ -17,26 +11,6 @@ return {
 					"vim",
 					"vimdoc",
 					"query",
-
-					"tsx",
-					"json",
-					"jsdoc",
-					"javascript",
-					"typescript",
-					"yaml",
-					"html",
-					"css",
-					"prisma",
-					"markdown",
-					"markdown_inline",
-					"svelte",
-					"graphql",
-					"bash",
-					"gitignore",
-					"dockerfile",
-
-					-- NOTE: project specific languages
-					"php",
 				},
 				auto_install = true,
 				highlight = {
