640afb43d46806a7f309d4be2777adaec5965c05
sabovyan <abovyan.sargis90@gmail.com>
2024-03-21 23:16:34 +0400

    [[markdown]] add wrap for blog files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,3 +3,9 @@ vim.opt_local.conceallevel = 2
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "en_us"
 
+vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPre" }, {
+	pattern = "blog/*.md",
+	callback = function()
+		vim.opt_local.wrap = true
+	end,
+})
