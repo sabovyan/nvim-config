1c591ab099d454cb59fd87e0189597ca2c7c5f51
sabovyan <abovyan.sargis90@gmail.com>
2024-03-23 00:35:18 +0400

    [[user_cmd]] addd redir to write cmd result into file

--- lua/configs/autocmd.lua
+++ lua/configs/autocmd.lua
@@ -86,3 +86,11 @@ vim.api.nvim_create_autocmd("TermOpen", {
 		vim.opt_local.relativenumber = false
 	end,
 })
+
+-- TODO: make sure always to write to the same buffer
+vim.api.nvim_create_user_command("Redir", function(ctx)
+	local lines = vim.split(vim.api.nvim_exec(ctx.args, true), "\n", { plain = true })
+	vim.cmd("new")
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+	vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
