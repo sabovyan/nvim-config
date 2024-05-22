80702f974ebfb7bb979dcf61c0172eb7b17ccdba
sabovyan <abovyan.sargis90@gmail.com>
2024-05-22 16:37:21 +0400

    [[commenting]] replace numToStr with folke/comment

--- lua/plugins/helpers/basic.lua
+++ lua/plugins/helpers/basic.lua
@@ -86,19 +86,11 @@ return {
 			},
 		},
 	},
-
 	{
-		"numToStr/Comment.nvim",
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-
-		lazy = false,
-		config = function()
-			vim.g.skip_ts_context_commentstring_module = true
-
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
-		end,
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
 	},
 
 	-- INFO: show colors based on hex
