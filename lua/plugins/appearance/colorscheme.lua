152e13c86bf643ed89e4d67689db2a937f960bde
sabovyan <abovyan.sargis90@gmail.com>
2024-06-14 20:30:24 +0400

    add everforest

--- lua/plugins/appearance/colorscheme.lua
+++ lua/plugins/appearance/colorscheme.lua
@@ -1,5 +1,23 @@
 return {
 	{
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
+		config = function()
+			require("everforest").setup({
+				background = "hard",
+				italics = true,
+				dim_inactive_windows = true,
+				diagnostic_text_highlight = true,
+				ui_contrast = "high",
+			})
+
+			vim.cmd([[colorscheme everforest]])
+		end,
+	},
+	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		lazy = false,
@@ -67,7 +85,7 @@ return {
 				end,
 			})
 
-			vim.cmd.colorscheme("catppuccin")
+			-- vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
