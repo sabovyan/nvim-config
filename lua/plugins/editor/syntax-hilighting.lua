23d3c52daae86b165ec9ebee839ef73bca6a9247
sabovyan <abovyan.sargis90@gmail.com>
2024-06-17 10:04:43 +0400

    [[syntax highlighting]] disable latex

--- lua/plugins/editor/syntax-hilighting.lua
+++ lua/plugins/editor/syntax-hilighting.lua
@@ -39,7 +39,9 @@ return {
 		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
-			require("render-markdown").setup({})
+			require("render-markdown").setup({
+				latex_enabled = false,
+			})
 		end,
 	},
 }
