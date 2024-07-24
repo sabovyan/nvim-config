0bd6ef54e80d9a84f7562f517953b502180bc76f
sabovyan <abovyan.sargis90@gmail.com>
2024-07-24 22:01:17 +0400

    [[lsp_on_attach]] update depricated feature

--- lua/configs/autocmd.lua
+++ lua/configs/autocmd.lua
@@ -89,7 +89,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 
 -- TODO: make sure always to write to the same buffer
 vim.api.nvim_create_user_command("Redir", function(ctx)
-	local lines = vim.split(vim.api.nvim_exec(ctx.args, true), "\n", { plain = true })
+	local lines = vim.split(vim.api.nvim_exec2(ctx.args, true), "\n", { plain = true })
 	vim.cmd("new")
 	vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
 	vim.opt_local.modified = false
