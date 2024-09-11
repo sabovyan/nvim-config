741a37690d875a69310f78d064ac62c9b9f251b3
sabovyan <abovyan.sargis90@gmail.com>
2024-09-11 14:33:35 +0400

    [[format]] add jsonc

--- lua/plugins/editor/format.lua
+++ lua/plugins/editor/format.lua
@@ -52,6 +52,7 @@ return {
 				css = { "prettier" },
 				html = { "prettier" },
 				json = { "prettier" },
+				jsonc = { "prettier" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
 				graphql = { "prettier" },
