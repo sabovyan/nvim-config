6e2508ef16e89af0efa578f87a8f58fa48505f84
sabovyan <abovyan.sargis90@gmail.com>
2024-12-20 09:04:18 +0400

    [[cmp]] move to blink

---
+++ lua/plugins/editor/blink.cmp.lua
@@ -0,0 +1,87 @@
+return {
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+					require("luasnip.loaders.from_vscode").lazy_load({
+						paths = { vim.fn.stdpath("config") .. "/snippets" },
+					})
+				end,
+			},
+		},
+	},
+	{
+
+		"saghen/blink.cmp",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"folke/lazydev.nvim",
+			{ "L3MON4D3/LuaSnip", version = "v2.*" },
+		},
+
+		-- use a release tag to download pre-built binaries
+		version = "v0.*",
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = { preset = "enter" },
+
+			completion = {
+				auto_brackets = {
+					enabled = true,
+				},
+			},
+
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				-- optionally disable cmdline completions
+				-- cmdline = {},
+				providers = {
+					-- dont show LuaLS require statements when lazydev has items
+					lsp = { fallback_for = { "lazydev" } },
+					lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+				},
+			},
+			snippets = {
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
+			},
+
+			-- experimental signature help support
+			signature = { enabled = true },
+		},
+		-- allows extending the providers array elsewhere in your config
+		-- without having to redefine it
+		opts_extend = { "sources.default" },
+	},
+}
--- lua/plugins/editor/cmp.lua
+++ lua/plugins/editor/cmp.lua
@@ -1,147 +1,146 @@
 return {
-
-	{
-		"hrsh7th/nvim-cmp",
-		version = false, -- last release is way too old
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lua",
-			"hrsh7th/cmp-emoji",
-			"onsails/lspkind.nvim",
-			{
-				"folke/lazydev.nvim",
-				ft = "lua", -- only load on lua files
-				opts = {
-					library = {
-						-- See the configuration section for more details
-						-- Load luvit types when the `vim.uv` word is found
-						{ path = "luvit-meta/library", words = { "vim%.uv" } },
-					},
-				},
-				dependencies = { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-			},
-
-			-- NOTE alternative "monkoose/neocodeium",
-
-			-- {
-			-- 	"Exafunction/codeium.nvim",
-			-- 	dependencies = {
-			-- 		"nvim-lua/plenary.nvim",
-			-- 		"hrsh7th/nvim-cmp",
-			-- 	},
-			-- config = function()
-			-- 	require("codeium").setup({})
-			-- end,
-			-- },
-
-			-- {
-			-- 	"zbirenbaum/copilot-cmp",
-			-- 	dependencies = {
-			-- 		"zbirenbaum/copilot.lua",
-			-- 		config = function()
-			-- 			require("copilot").setup({
-			-- 				suggestion = { enabled = false },
-			-- 				panel = { enabled = false },
-			-- 				filetypes = {
-			-- 					markdown = true,
-			-- 					help = true,
-			-- 				},
-			-- 			})
-			-- 		end,
-			-- 	},
-			-- 	config = function()
-			-- 		require("copilot_cmp").setup()
-			-- 	end,
-			-- },
-		},
-		config = function()
-			local cmp = require("cmp")
-			local defaults = require("cmp.config.default")()
-
-			require("configs.snippet").register_cmp_source()
-
-			cmp.setup({
-				completion = {
-					completeopt = "menu,menuone,noinsert",
-				},
-				-- snippet = {
-				-- 	expand = function(arg)
-				-- 		print(arg)
-				-- 		vim.snippet.expand(arg.body)
-				-- 	end,
-				-- },
-				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item({
-						behavior = cmp.SelectBehavior.Insert,
-					}),
-					["<C-p>"] = cmp.mapping.select_prev_item({
-						behavior = cmp.SelectBehavior.Insert,
-					}),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-					["<S-CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-					["<C-CR>"] = function(fallback)
-						cmp.abort()
-						fallback()
-					end,
-				}),
-
-				sources = cmp.config.sources({
-					{ name = "snp", group_index = 2 },
-					{
-						name = "lazydev",
-						group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-					},
-					{ name = "nvim_lsp", group_index = 1 },
-					{ name = "nvim_lua", group_index = 1, max_item_count = 5 },
-					-- { name = "copilot", group_index = 2, max_item_count = 5 },
-					-- { name = "codeium", group_index = 2, max_item_count = 5 },
-					{ name = "path", gorud_index = 2, max_item_count = 5 },
-					{ name = "emoji", group_index = 3, max_item_count = 2 },
-				}, {
-					{ name = "buffer", max_item_count = 5 },
-				}),
-				formatting = {
-					fields = { "kind", "abbr", "menu" },
-					format = function(entry, vim_item)
-						local kind = require("lspkind").cmp_format({
-							mode = "symbol_text",
-							maxwidth = 50,
-							ellipsis_char = "...",
-							-- menu = {
-							-- 	buffer = "[Buffer]",
-							-- 	nvim_lsp = "[LSP]",
-							-- 	luasnip = "[LuaSnip]",
-							-- 	nvim_lua = "[Lua]",
-							-- 	snp = "[Snippet]",
-							-- 	-- codeium = "[Codeium]",
-							-- 	-- copilot = "[Copilot]",
-							-- },
-							-- symbol_map = {
-							-- 	Codeium = "",
-							-- 	-- 	-- Copilot = ""
-							-- },
-						})(entry, vim_item)
-
-						local strings = vim.split(kind.kind, "%s", { trimempty = true })
-						kind.kind = " " .. (strings[1] or "") .. " "
-						kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-						return kind
-					end,
-				},
-				sorting = defaults.sorting,
-			})
-		end,
-	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	version = false, -- last release is way too old
+	-- 	event = "InsertEnter",
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		"hrsh7th/cmp-buffer",
+	-- 		"hrsh7th/cmp-path",
+	-- 		"saadparwaiz1/cmp_luasnip",
+	-- 		"hrsh7th/cmp-nvim-lua",
+	-- 		"hrsh7th/cmp-emoji",
+	-- 		"onsails/lspkind.nvim",
+	-- 		{
+	-- 			"folke/lazydev.nvim",
+	-- 			ft = "lua", -- only load on lua files
+	-- 			opts = {
+	-- 				library = {
+	-- 					-- See the configuration section for more details
+	-- 					-- Load luvit types when the `vim.uv` word is found
+	-- 					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+	-- 				},
+	-- 			},
+	-- 			dependencies = { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+	-- 		},
+	--
+	-- 		-- NOTE alternative "monkoose/neocodeium",
+	--
+	-- 		-- {
+	-- 		-- 	"Exafunction/codeium.nvim",
+	-- 		-- 	dependencies = {
+	-- 		-- 		"nvim-lua/plenary.nvim",
+	-- 		-- 		"hrsh7th/nvim-cmp",
+	-- 		-- 	},
+	-- 		-- config = function()
+	-- 		-- 	require("codeium").setup({})
+	-- 		-- end,
+	-- 		-- },
+	--
+	-- 		-- {
+	-- 		-- 	"zbirenbaum/copilot-cmp",
+	-- 		-- 	dependencies = {
+	-- 		-- 		"zbirenbaum/copilot.lua",
+	-- 		-- 		config = function()
+	-- 		-- 			require("copilot").setup({
+	-- 		-- 				suggestion = { enabled = false },
+	-- 		-- 				panel = { enabled = false },
+	-- 		-- 				filetypes = {
+	-- 		-- 					markdown = true,
+	-- 		-- 					help = true,
+	-- 		-- 				},
+	-- 		-- 			})
+	-- 		-- 		end,
+	-- 		-- 	},
+	-- 		-- 	config = function()
+	-- 		-- 		require("copilot_cmp").setup()
+	-- 		-- 	end,
+	-- 		-- },
+	-- 	},
+	-- 	config = function()
+	-- 		local cmp = require("cmp")
+	-- 		local defaults = require("cmp.config.default")()
+	--
+	-- 		require("configs.snippet").register_cmp_source()
+	--
+	-- 		cmp.setup({
+	-- 			completion = {
+	-- 				completeopt = "menu,menuone,noinsert",
+	-- 			},
+	-- 			-- snippet = {
+	-- 			-- 	expand = function(arg)
+	-- 			-- 		print(arg)
+	-- 			-- 		vim.snippet.expand(arg.body)
+	-- 			-- 	end,
+	-- 			-- },
+	-- 			mapping = cmp.mapping.preset.insert({
+	-- 				["<C-n>"] = cmp.mapping.select_next_item({
+	-- 					behavior = cmp.SelectBehavior.Insert,
+	-- 				}),
+	-- 				["<C-p>"] = cmp.mapping.select_prev_item({
+	-- 					behavior = cmp.SelectBehavior.Insert,
+	-- 				}),
+	-- 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+	-- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+	-- 				["<C-Space>"] = cmp.mapping.complete(),
+	-- 				["<C-e>"] = cmp.mapping.abort(),
+	-- 				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	-- 				["<S-CR>"] = cmp.mapping.confirm({
+	-- 					behavior = cmp.ConfirmBehavior.Replace,
+	-- 					select = true,
+	-- 				}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	-- 				["<C-CR>"] = function(fallback)
+	-- 					cmp.abort()
+	-- 					fallback()
+	-- 				end,
+	-- 			}),
+	--
+	-- 			sources = cmp.config.sources({
+	-- 				{ name = "snp", group_index = 2 },
+	-- 				{
+	-- 					name = "lazydev",
+	-- 					group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+	-- 				},
+	-- 				{ name = "nvim_lsp", group_index = 1 },
+	-- 				{ name = "nvim_lua", group_index = 1, max_item_count = 5 },
+	-- 				-- { name = "copilot", group_index = 2, max_item_count = 5 },
+	-- 				-- { name = "codeium", group_index = 2, max_item_count = 5 },
+	-- 				{ name = "path", gorud_index = 2, max_item_count = 5 },
+	-- 				{ name = "emoji", group_index = 3, max_item_count = 2 },
+	-- 			}, {
+	-- 				{ name = "buffer", max_item_count = 5 },
+	-- 			}),
+	-- 			formatting = {
+	-- 				fields = { "kind", "abbr" },
+	-- 				format = function(entry, vim_item)
+	-- 					local kind = require("lspkind").cmp_format({
+	-- 						mode = "symbol_text",
+	-- 						maxwidth = 50,
+	-- 						ellipsis_char = "...",
+	-- 						-- menu = {
+	-- 						-- 	buffer = "[Buffer]",
+	-- 						-- 	nvim_lsp = "[LSP]",
+	-- 						-- 	luasnip = "[LuaSnip]",
+	-- 						-- 	nvim_lua = "[Lua]",
+	-- 						-- 	snp = "[Snippet]",
+	-- 						-- 	-- codeium = "[Codeium]",
+	-- 						-- 	-- copilot = "[Copilot]",
+	-- 						-- },
+	-- 						-- symbol_map = {
+	-- 						-- 	Codeium = "",
+	-- 						-- 	-- 	-- Copilot = ""
+	-- 						-- },
+	-- 					})(entry, vim_item)
+	--
+	-- 					local strings = vim.split(kind.kind, "%s", { trimempty = true })
+	-- 					kind.kind = " " .. (strings[1] or "") .. " "
+	-- 					kind.menu = "    (" .. (strings[2] or "") .. ")"
+	--
+	-- 					return kind
+	-- 				end,
+	-- 			},
+	-- 			sorting = defaults.sorting,
+	-- 		})
+	-- 	end,
+	-- },
 }
