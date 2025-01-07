51808de351193d571d10963330fabf974425923c
sabovyan <abovyan.sargis90@gmail.com>
2025-01-07 11:21:11 +0400

    [[snaks]] disable notifier

--- lua/plugins/helpers/snacks.lua
+++ lua/plugins/helpers/snacks.lua
@@ -9,7 +9,7 @@ return {
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 			dashboard = { enabled = true },
-			notifier = { enabled = true },
+			-- notifier = { enabled = true },
 			-- scroll = { enabled = true },
 			words = { enabled = true },
 
