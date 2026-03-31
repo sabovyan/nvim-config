4a4efa979b0c97c9073e79df70cbcfc2e96c7371
sabovyan <abovyan.sargis90@gmail.com>
2026-03-31 10:27:23 +0400

    comment dynamic theme

--- init.lua
+++ init.lua
@@ -26,25 +26,25 @@ vim.cmd.colorscheme("gruvbox")
 --
 --
 --
--- if macos_appearance() == "dark" then
---   vim.o.background = "dark"
+--  if macos_appearance() == "dark" then
+--    vim.o.background = "dark"
 --
---   vim.cmd.colorscheme("rose-pine")
--- else
---   vim.o.background = "light"
---   vim.o.cursorline = false
+--    -- vim.cmd.colorscheme("rose-pine")
+--  else
+--    vim.o.background = "light"
+--    vim.o.cursorline = false
 --
---   vim.cmd("colorscheme rose-pine")
+--    -- vim.cmd("colorscheme rose-pine")
 --
---   -- NOTE this are needed for light mode default theme
---   vim.opt.guicursor = {
---     -- "n-v-c:block-Cursor/lCursor-blinkon500-blinkoff500",
---     "i-ci:ver25-CursorInsert-blinkon500-blinkoff500",
---     "r-cr:hor20-CursorReplace-blinkon500-blinkoff500",
---   }
---   -- local main_cursor_color='#BEFC65'
---   -- vim.api.nvim_set_hl(0, "Cursor", { fg = "black", bg = main_cursor_color })
---   -- vim.api.nvim_set_hl(0, "CursorInsert", { fg = "black", bg = main_cursor_color })
---   -- vim.api.nvim_set_hl(0, "CursorReplace", { fg = "black", bg = main_cursor_color })
--- end
+--    -- NOTE this are needed for light mode default theme
+--    -- vim.opt.guicursor = {
+--      -- "n-v-c:block-Cursor/lCursor-blinkon500-blinkoff500",
+--      -- "i-ci:ver25-CursorInsert-blinkon500-blinkoff500",
+--      -- "r-cr:hor20-CursorReplace-blinkon500-blinkoff500",
+--    -- }
+--    -- local main_cursor_color='#BEFC65'
+--    -- vim.api.nvim_set_hl(0, "Cursor", { fg = "black", bg = main_cursor_color })
+--    -- vim.api.nvim_set_hl(0, "CursorInsert", { fg = "black", bg = main_cursor_color })
+--    -- vim.api.nvim_set_hl(0, "CursorReplace", { fg = "black", bg = main_cursor_color })
+--  end
 --
