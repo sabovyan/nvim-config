d12d4b74e1147f93a7d39866924815d08771b17c
sabovyan <abovyan.sargis90@gmail.com>
2026-04-07 13:57:15 +0400

    update opencode

--- lua/plugins/opencode.nvim.lua
+++ lua/plugins/opencode.nvim.lua
@@ -1,30 +1,20 @@
 return {
   "NickvanDyke/opencode.nvim",
-  config = function()
-    ---@type opencode.Opts
-    vim.g.opencode_opts = {
-      -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
-      provider = {
-        enabled = "snacks", -- Default when `snacks.terminal` is enabled.
-        -- snacks = {
-        --   -- Customize `snacks.terminal` to your liking.
-        -- }
-      }
-    }
 
-    -- Required for `opts.auto_reload`.
-    vim.o.autoread = true
+  config = function()
+
 
     -- Recommended/example keymaps.
-    vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode" })
+    vim.keymap.set({ "n", "x" }, "<C-S-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode" })
     vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end, { desc = "Execute opencode action…" })
     vim.keymap.set({ "n", "x" }, "ga",    function() require("opencode").prompt("@this") end, { desc = "Add to opencode" })
     vim.keymap.set({ "n", "t" }, "<C-.>",   function() require("opencode").toggle() end, { desc = "Toggle opencode" })
-    vim.keymap.set("n", "<S-C-u>", 
+    vim.keymap.set("n", "<S-C-u>",
     function() require("opencode").command("session.half.page.up") end, { desc = "opencode half page up" })
-    vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "opencode half page down" })
-    -- -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
-    -- vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment', noremap = true })
-    -- vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement', noremap = true })
+    vim.keymap.set("n", "<S-C-d>", function()
+      require("opencode").command("session.half.page.down")
+    end, { desc = "opencode half page down" })
+
+
   end,
 }
